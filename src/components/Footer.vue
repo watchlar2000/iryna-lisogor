f7a75dbf79794e7ce883f91517c72ad7429c2f83
Sergii Kochetov <sergii.koch@gmail.com>
2023-04-06 22:56:53 +0300

    Update footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,15 @@
 <script>
+import IconBehance from "./icons/IconBehance.vue";
+import IconInstagram from "./icons/IconInstagram.vue";
+import IconMail from "./icons/IconMail.vue";
+
 export default {
   name: "AppFooter",
+  components: {
+    IconBehance,
+    IconInstagram,
+    IconMail,
+  },
   computed: {
     fullYear() {
       return new Date().getFullYear();
@@ -11,9 +20,34 @@ export default {
 
 <template>
   <footer class="footer">
-    <p>All artistic content Copyright &#169;</p>
-    <p>{{ fullYear }} Iryna Lisogor, All rights</p>
+    <div class="socials-list">
+      <a href="https://www.instagram.com/iryna_lisogor/" target="_blank"
+        ><IconInstagram :width="50"
+      /></a>
+      <a href="https://www.behance.net/lisogorka8741b" target="_blank"
+        ><IconBehance :width="50"
+      /></a>
+      <a href="mailto:iryna.lisogor.artwrk@gmail.com" target="_blank"
+        ><IconMail :width="50"
+      /></a>
+    </div>
+    <div>
+      <p>All artistic content Copyright &#169;</p>
+      <p>{{ fullYear }} Iryna Lisogor, All rights</p>
+    </div>
   </footer>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.footer {
+  color: var(--color-title);
+  font-size: 14px;
+}
+.socials-list {
+  display: flex;
+  gap: 18px;
+  width: 100%;
+  justify-content: flex-end;
+  padding-right: 24px;
+}
+</style>
